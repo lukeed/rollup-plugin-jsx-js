1fc9b76214bdee400fa31cd57a09ba9ead74a06d
Luke Edwards <luke.edwards05@gmail.com>
2016-12-13 22:41:29 -0800

    finalize plugin logic

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-'use strict';
-module.exports = (input, opts) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
-	}
+const jsx = require('jsx.js');
+const MagicString = require('magic-string');
+const createFilter = require('rollup-pluginutils').createFilter;
 
+module.exports = function (opts) {
 	opts = opts || {};
 
-	return input + ' & ' + (opts.postfix || 'rainbows');
+	const filter = createFilter(opts.include, opts.exclude);
+	const precise = opts.precise || false;
+	// const pragma = opts.pragma;
+
+	['precise', 'include', 'exclude'].forEach(k => delete opts[k]);
+
+	return {
+		name: 'jsx',
+		transform(code, id) {
+			if (!filter(id)) {
+				return null;
+			}
+
+			const out = jsx(code, opts);
+			const str = new MagicString(code);
+			str.overwrite(0, code.length, out.toString());
+
+			return {
+				code: str.toString(),
+				map: str.generateMap({hires: precise})
+			};
+		}
+	};
 };
